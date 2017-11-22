fe35b87473e58e9e06edfbc23b20271fb8e8ad79
asjkl <gionny930@gmail.com>
2017-11-22 15:14:06 +0100

    commit

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -3,6 +3,7 @@ package it.unical.test;
 import java.time.Instant;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,20 +13,28 @@ public class AthleticGameBasicTest {
 	
 	private static AthleticGame game;
 	
+	@Before
+	public void before(){
+		System.out.println("ciao3");
+	}
+	
 	@Test
 	public void testTaglia(){
+		System.out.println("ciao2");
 		game.addArrival("Sapia", Instant.now());
 		Assert.assertEquals(1,game.getArrivals().size());
 	}
 	
 	@Test(expected=Exception.class)
 	public void controlloPartecipante(){
+		System.out.println("ciao1");
 		game.addArrival("Giovanni", Instant.now());
 		game.getParecipiantTime("Giovanni");
 	}
 	
 	@BeforeClass
 	public static void beforeClass(){
+		System.out.println("ciao");
 		game=new AthleticGame("Ciclismo");
 	}
 }
